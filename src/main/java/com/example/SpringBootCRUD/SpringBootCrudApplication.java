7505511687bf7292ba0617e56fb4d6b64ec2c3e3
lqh1806 <lequochung1806@gmail.com>
2022-07-25 16:47:47 +0700

    maria_db

--- src/main/java/com/example/SpringBootCRUD/SpringBootCrudApplication.java
+++ src/main/java/com/example/SpringBootCRUD/SpringBootCrudApplication.java
@@ -3,11 +3,12 @@ package com.example.SpringBootCRUD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootCrudApplication {
+public class SpringBootCrudApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(SpringBootCrudApplication.class, args);
 	}
 
--- src/main/java/com/example/SpringBootCRUD/model/Employee.java
+++ src/main/java/com/example/SpringBootCRUD/model/Employee.java
@@ -10,8 +10,6 @@ import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "employees")
 public class Employee {
 
